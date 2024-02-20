5c4827478eaef17da30ce352204a9d5078f481ae
BlakeJC94 <blakejamescook@gmail.com>
2024-02-20 11:27:07 +1100

    Removed causal option for now

--- torchaudio_filters/filters.py
+++ torchaudio_filters/filters.py
@@ -28,14 +28,13 @@ def _filter_coeffs(
 
 
 class _BaseFilter(nn.Module):
-    def __init__(self, b, a, causal: bool = False):
+    def __init__(self, b: torch.Tensor, a: torch.Tensor):
         super().__init__()
         self.register_buffer("b", b)
         self.register_buffer("a", a)
-        self.causal = causal
 
     @staticmethod
-    def _pad_odd(x, n):
+    def _odd_ext(x, n):
         left_end = x[..., :1]
         left_ext = x[..., 1:n+1].flip(dims=(-1,))
 
@@ -56,28 +55,22 @@ class _BaseFilter(nn.Module):
         x = x / scale
 
         padlen = 3 * max(len(self.a), len(self.b))
-        x = self._pad_odd(x, padlen)
+        x = self._odd_ext(x, padlen)
 
-        if self.causal:
-            x = lfilter(x, self.a, self.b, clamp=False, batching=True)
-        else:
-            # x = filtfilt(x, self.a, self.b)
-            x = lfilter(x, self.a, self.b)
-            x = lfilter(x.flip(dims=(-1,)), self.a, self.b).flip(dims=(-1,))
+        x = filtfilt(x, self.a, self.b)
 
         x = x[..., padlen:-padlen]
         return x * scale
 
 
 class LowPass(_BaseFilter):
-    def __init__(self, cutoff, sample_rate, order=2, causal=False):
+    def __init__(self, cutoff: float, sample_rate: float, order=2):
         """Constructor for the LowPass class.
 
         Args:
             cutoff: Cutoff frequency in hertz.
             sample_rate: Input sampling rate in hertz.
             order: Degree of polynomial in filter (default = 2)
-            causal: If True, apply filter in a causal manner.
         """
         b, a = _filter_coeffs(
             "lowpass",
@@ -85,37 +78,37 @@ class LowPass(_BaseFilter):
             [cutoff],
             sample_rate,
         )
-        super().__init__(b, a, causal)
+        super().__init__(b, a)
 
 
 class HighPass(_BaseFilter):
-    def __init__(self, cutoff, sample_rate, order=2, causal=False):
+    def __init__(self, cutoff, sample_rate, order=2):
         b, a = _filter_coeffs(
             "highpass",
             order,
             [cutoff],
             sample_rate,
         )
-        super().__init__(b, a, causal)
+        super().__init__(b, a)
 
 
 class BandPass(_BaseFilter):
-    def __init__(self, cutoff_low, cutoff_high, sample_rate, order=2, causal=False):
+    def __init__(self, cutoff_low, cutoff_high, sample_rate, order=2):
         b, a = _filter_coeffs(
             "band",
             order,
             [cutoff_low, cutoff_high],
             sample_rate,
         )
-        super().__init__(b, a, causal)
+        super().__init__(b, a)
 
 
 class Notch(_BaseFilter):
-    def __init__(self, cutoff_low, cutoff_high, sample_rate, order=2, causal=False):
+    def __init__(self, cutoff_low, cutoff_high, sample_rate, order=2):
         b, a = _filter_coeffs(
             "bandstop",
             order,
             [cutoff_low, cutoff_high],
             sample_rate,
         )
-        super().__init__(b, a, causal)
+        super().__init__(b, a)
